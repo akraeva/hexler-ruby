0a6a7f3f468d4334a85bb04d0b2127053cab694a
akraeva <adkraeva@gmail.com>
2022-07-20 22:12:20 +0300

    my CB problem solving L29-L30

--- code-basics/L29.rb
+++ code-basics/L29.rb
@@ -1,2 +1,27 @@
 #!/usr/bin/env ruby
 
+def gen_diff(hash_old, hash_new)
+    old_keys = hash_old.keys #собрать ключи можно было умнее
+    new_keys = hash_new.keys
+    result ={}
+    (old_keys - new_keys).each { |key| result[key] = "deleted"}
+    (old_keys & new_keys).each do |key| 
+        result[key] = hash_old[key] != hash_new[key] ? "changed" : "unchanged"
+    end
+    (new_keys - old_keys).each { |key| result[key] = "added"}
+    result
+end
+
+#test 
+
+result = gen_diff(
+            { one: 'eon', two: 'two', four: true },
+            { two: 'own', zero: 4, four: true }
+        );
+example = {one: 'deleted', 
+            two: 'changed', 
+            four: 'unchanged', 
+            zero: 'added'
+        }
+
+puts  result == example
--- code-basics/L30.rb
+++ code-basics/L30.rb
@@ -1,2 +1,13 @@
 #!/usr/bin/env ruby
 
+def valid_date?(date)
+    day, month = date[0,5].split "-"
+    (1..31).to_a.include?(day.to_i) && (1..12).to_a.include?(month.to_i)   
+end
+
+
+
+puts valid_date?('11-11-2011') 
+puts valid_date?('13-11-2011') 
+puts !valid_date?('11-13-2011')
+puts !valid_date?('55-11-2011')
